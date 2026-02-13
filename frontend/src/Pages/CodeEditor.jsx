f2cc57f0b7a838624a4529bd4fb33664c26eb91f
Kaushal Sharma <Ksharma17052005@gmail.com>
2026-02-13 22:40:38 +0530

    Updated Production Url

--- frontend/src/Pages/CodeEditor.jsx
+++ frontend/src/Pages/CodeEditor.jsx
@@ -28,7 +28,7 @@ int main() {
 }
 `,
 };
-const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5001";
+const API_URL = import.meta.env.VITE_API_URL || "";
 
 const CodeEditor = ({ theme, language, code, setCode }) => {
   const [input, setInput] = useState("");
